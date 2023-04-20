2a9de1dad2856ac41c86ce3919009129ca8f2928
Nathaniel Middleton <nathanielm@alleninstitute.org>
2023-04-20 15:38:46 -0700

    adding total memory

--- free_resources.py
+++ free_resources.py
@@ -6,9 +6,9 @@ print(type(data))
 
 for nodes in data['nodes']:
     print('Node', nodes['name'], 'has', nodes['cpus']-nodes['alloc_cpus'], 'cpus free, \n approximately', \
-            nodes['real_memory']-nodes['alloc_memory'], 'MB memory available \n', \
+            nodes['real_memory']-nodes['alloc_memory'], 'MB memory available out of a total of', nodes['real_memory'], 'MB\n', \
             nodes['gres'], 'with', nodes['gres_used'], 'currently used GPUs \n')
-            
+ 
 #    if nodes['alloc_memory']/nodes['real_memory'] >=.8:
 #        print(nodes['name'], 'mem more than 80% used at', nodes['alloc_memory']/nodes['real_memory'] )
 #        consumed = 'mem'
