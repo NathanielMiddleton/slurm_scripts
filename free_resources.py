f6fcd4b46836ad36a98ef328483a8db14185a016
Nathaniel Middleton <nathanielm@alleninstitute.org>
2023-04-21 10:40:52 -0700

    rewriting free resources

--- free_resources.py
+++ free_resources.py
@@ -1,17 +1,44 @@
+import subprocess
 import json
 
-with open('sinfo.json') as json_file:
-    data = json.load(json_file)
-print(type(data))
+# Run sinfo --json and capture the output
+output = subprocess.run(["sinfo", "--json"], 
+capture_output=True)
 
-for nodes in data['nodes']:
-    print('Node', nodes['name'], 'has', nodes['cpus']-nodes['alloc_cpus'], 'cpus free, \n approximately', \
-            nodes['real_memory']-nodes['alloc_memory'], 'MB memory available out of a total of', nodes['real_memory'], 'MB\n', \
-            nodes['gres'], 'with', nodes['gres_used'], 'currently used GPUs \n')
- 
-#    if nodes['alloc_memory']/nodes['real_memory'] >=.8:
-#        print(nodes['name'], 'mem more than 80% used at', nodes['alloc_memory']/nodes['real_memory'] )
-#        consumed = 'mem'
-#    elif nodes['alloc_cpus']/nodes['cpus'] >=.8:
-#        print(nodes['name'], 'cpus more than 80% with', nodes['cpus']-nodes['alloc_cpus'], 'remaining')
-#
+# Check if the command was successful
+if output.returncode == 0:
+    # Parse the json output
+    data = json.loads(output.stdout)
+
+    # Loop through the nodes and print their details
+    for node in data["nodes"]:
+        # Get the node name, state, allocated memory and total memory
+        name = node["name"]
+        state = node["state"]
+        alloc_mem = node["alloc_memory"]
+        total_mem = node["real_memory"]
+        total_cpus = node["cpus"]
+        alloc_cpus = node["alloc_cpus"]
+        total_gres = node["gres"]
+        alloc_gres = node["gres_used"]
+
+       "gres": "gpu:a100:1",
+       "gres_used": "gpu:a100:1(IDX:0)"
+
+        # Calculate the percentage of consumed memory
+        percent_mem = round(alloc_mem / total_mem * 100, 2)
+        available_cpus = total_cpus - alloc_cpus
+
+        # Print the details
+        print(f"Node: {name}")
+        print(f"State: {state}")
+        print(f"Allocated memory: {alloc_mem} MB")
+        print(f"Total memory: {total_mem} MB")
+        print(f"Percentage of consumed memory: {percent_mem}%")
+        print(f"Available CPUs: {available_cpus} out of {total_cpus}")
+        if total_gres:
+            print(f"{alloc_gres})
+        print()
+else:
+# Print an error message if the command failed
+    print(f"Error: sinfo --json failed with code {output.returncode}")
